4c789881c989049c923e5fe18adf5e27faff498b
sabakantaro <sbkryo290112@yahoo.co.jp>
2023-06-24 11:15:57 -0700

    Add a file

--- js/reverse_bits.js
+++ js/reverse_bits.js
@@ -5,7 +5,7 @@ Note:
 
 Note that in some languages, such as Java, there is no unsigned integer type. In this case, both input and output will be given as a signed integer type. They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
 In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.
- 
+
 
 Example 1:
 
@@ -17,12 +17,12 @@ Example 2:
 Input: n = 11111111111111111111111111111101
 Output:   3221225471 (10111111111111111111111111111111)
 Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
- 
+
 
 Constraints:
 
 The input must be a binary string of length 32
- 
+
 
 Follow up: If this function is called many times, how would you optimize it?
 */
