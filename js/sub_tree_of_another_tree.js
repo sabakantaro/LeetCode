e44feba4063abb45e79248e214f3fa936b409362
sabakantaro <sbkryo290112@yahoo.co.jp>
2023-07-17 10:10:14 -0700

    Modify a file

--- js/sub_tree_of_another_tree.js
+++ js/sub_tree_of_another_tree.js
@@ -25,14 +25,28 @@ The number of nodes in the subRoot tree is in the range [1, 1000].
 -104 <= subRoot.val <= 104
 */
 
+// var isSubtree = function (root, subRoot) {
+//   if (!root) return false;
+//   if (dfs(root, subRoot)) return true;
+
+//   function dfs(node1, node2) {
+//     if (!node1 && !node2) return true;
+//     if ((node1 && !node2) || (node2 && !node1) || node1.val !== node2.val)
+//       return false;
+//     return dfs(node1.left, node2.left) && dfs(node1.right, node2.right);
+//   }
+
+//   return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+// };
+
 var isSubtree = function (root, subRoot) {
   if (!root) return false;
   if (dfs(root, subRoot)) return true;
 
   function dfs(node1, node2) {
     if (!node1 && !node2) return true;
-    if ((node1 && !node2) || (node2 && !node1) || node1.val !== node2.val)
-      return false;
+    if (!node1 || !node2 || node1.val !== node2.val) return false;
+
     return dfs(node1.left, node2.left) && dfs(node1.right, node2.right);
   }
 
