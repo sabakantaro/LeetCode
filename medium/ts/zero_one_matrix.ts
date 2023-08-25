1ac99625f59f78b7bb1da312f12336e9d567ecf7
sabakantaro <sbkryo290112@yahoo.co.jp>
2023-08-25 10:43:14 -0700

    Add a file

--- medium/ts/zero_one_matrix.ts
+++ medium/ts/zero_one_matrix.ts
@@ -1,38 +1,41 @@
 function updateMatrix(mat: number[][]): number[][] {
   const m = mat.length;
   const n = mat[0].length;
+  const que: [number, number][] = [];
+  const ans: number[][] = Array.from({ length: m }, () => Array(n).fill(0))
+  const directions: [number, number][] = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+  const visited = new Set();
 
-  // Initialize distance matrix and queue
-  const distance: number[][] = Array.from({ length: m }, () => Array(n).fill(Infinity));
-  const queue: [number, number][] = [];
-
-  // Directions: up, down, left, right
-  const directions: [number, number][] = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-
-  // Add positions of 0s to queue and set their distances to 0
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (mat[i][j] === 0) {
-        queue.push([i, j]);
-        distance[i][j] = 0;
+        que.push([i, j]);
+        visited.add({ m: i, n: j });
       }
     }
   }
 
-  // Breadth-first search
-  while (queue.length > 0) {
-    const [x, y] = queue.shift() as [number, number];
-    // Iterate through all four directions
-    for (const [dx, dy] of directions) {
-      const newX = x + dx;
-      const newY = y + dy;
-      // Check boundaries and update distances if necessary
-      if (newX >= 0 && newX < m && newY >= 0 && newY < n && distance[newX][newY] === Infinity) {
-        distance[newX][newY] = distance[x][y] + 1;
-        queue.push([newX, newY]);
+  let dist = 0;
+
+  while (que.length) {
+    for (let k = 0, len = que.length; k < len; k++) {
+      const [i, j] = que.shift() as [number, number];
+      if (mat[i][j] === 1) {
+        ans[i][j] = dist;
+      }
+      for (const d of directions) {
+        const ni = i + d[0];
+        const nj = j + d[1];
+        if (0 <= ni && ni < m && 0 <= nj && nj < n && !visited.has({ m: ni, n: nj })) {
+          que.push([ni, nj]);
+          visited.add({ m: ni, n: nj });
+        }
       }
     }
+
+    dist++
   }
 
-  return distance;
+  return ans
 }
+
