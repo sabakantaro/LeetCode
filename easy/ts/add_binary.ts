823936cf524300bfa072ab4bbe3c53b606267e8c
sabakantaro <sbkryo290112@yahoo.co.jp>
2024-01-27 09:13:10 -0800

    Update a file

--- easy/ts/add_binary.ts
+++ easy/ts/add_binary.ts
@@ -14,3 +14,28 @@ function addBinary(a: string, b: string): string {
 
   return result = carry ? carry + result : result;
 }
+
+
+function addBinary2(a: string, b: string): string {
+  const maxLength = Math.max(a.length, b.length);
+  const paddedA = a.padStart(maxLength, '0');
+  const paddedB = b.padStart(maxLength, '0');
+
+  let carry = 0;
+  let result = ''
+  for (let i = maxLength - 1; i >= 0; i--) {
+      const sum = parseInt(paddedA[i], 2) + parseInt(paddedB[i], 2) + carry;
+      carry = Math.floor(sum / 2)
+      result = (sum % 2).toString() + result;
+  }
+
+  return carry ? '1' + result : result;
+
+};
+
+/*
+Best Practices:
+
+Explicit Number Parsing: Use parseInt when you explicitly need an integer and know the expected radix.
+General Number Conversion: Use Number when you're unsure about the number type or want to handle both integers and floats.
+*/
