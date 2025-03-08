85d1a91d3c34b0ff8bb4dd85953fa4d80ba7094e
sabakantaro <sbkryo290112@yahoo.co.jp>
2025-03-08 00:24:17 -0500

    Add a file

---
+++ easy/ts/509.fibonacci-number.ts
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=509 lang=typescript
+ *
+ * [509] Fibonacci Number
+ */
+
+// @lc code=start
+function fib(n: number): number {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    if (n === 2) return 1;
+    return fib(n - 1) + fib(n - 2);
+};
+// @lc code=end
+
